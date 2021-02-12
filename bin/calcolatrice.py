b7c208d79e1ddea26b859834669a4b61a572b7cf
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-12 09:19:48 +0100

    Created bin/calcolatrice.py

---
+++ bin/calcolatrice.py
@@ -0,0 +1,19 @@
+"""calcolatrice
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-12"
+
+class calcolatrice:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(calcolatrice.calcolatrice())
+	
+	def calcolatrice():
+		"""calcolatrice first funtion
+		"""
+		return "calcolatrice" #for the test (see test_calcolatrice.py)
+		
+if __name__ == "__main__":
+	calcolatrice()
